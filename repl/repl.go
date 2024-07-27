c9abd903f3c9557699d4d020a37e0df9b0f3b61f
Roshan Lamichhane <lamichhanersn@gmail.com>
2024-07-27 17:42:56 +0545

    feat(): added a parser (based on Pratt Parser)

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/roshanlc/interpreter-in-go/lexer"
-	"github.com/roshanlc/interpreter-in-go/token"
+	"github.com/roshanlc/interpreter-in-go/parser"
 )
 
 // REPL = Read Evaluate Print Loop
@@ -30,10 +30,39 @@ func Start(in io.Reader, out io.Writer) {
 			fmt.Fprint(out, "Bye!!!\n")
 			return
 		}
-		l := lexer.New(line)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n\n", tok)
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
